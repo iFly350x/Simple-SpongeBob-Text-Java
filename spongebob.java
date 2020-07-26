43017c9dd0cb366835924e92a41bf7e8b63e7300
iFly350x <50535509+iFly350x@users.noreply.github.com>
2020-07-26 17:41:15 -0400

    Create spongebob.java

---
+++ spongebob.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class spongebobtext {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Sentence: ");
+        char[] sentence = input.nextLine().toCharArray();
+        boolean upper = true;
+        for (int i=0; i < sentence.length; i++) {
+            if (upper) {
+                sentence[i] = (char) Character.toUpperCase(sentence[i]);
+            } else {
+                sentence[i] = (char) Character.toLowerCase(sentence[i]);
+            }
+            upper = !upper;
+        }
+        System.out.println(sentence);
+
+    }
+}
